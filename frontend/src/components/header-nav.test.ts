22a1ea327e72d1146502bca814bd20cc5a958ee0
Brandon Mork <bpmork@ncsu.edu>
2022-12-17 18:38:30 -0500

    Add links to header-nav

--- frontend/src/components/header-nav.test.ts
+++ frontend/src/components/header-nav.test.ts
@@ -1,10 +1,42 @@
-// Write tests for the header-nav component
-
-import { render } from '@testing-library/vue';
+import { render, RenderOptions } from '@testing-library/vue';
+import { RouterLinkStub } from '@vue/test-utils';
 import HeaderNav from './header-nav.vue';
 
-test('renders the header-nav component', () => {
-  const { getByText } = render(HeaderNav);
+function _renderOptions(routePath: string): RenderOptions {
+  return {
+    props: {
+      routePath,
+    },
+    global: {
+      stubs: {
+        RouterLink: RouterLinkStub,
+      },
+    },
+  };
+}
 
-  expect(getByText('Click on the Vite and Vue logos to learn more')).toBeTruthy();
+test('renders the header-nav component', () => {
+  const { getByLabelText } = render(HeaderNav, _renderOptions('/'));
+  expect(getByLabelText('Main menu')).toBeTruthy();
+});
+
+test('Ulcerative Dermatitis link is active when current route is /', () => {
+  const { getByText } = render(HeaderNav, _renderOptions('/'));
+  expect(getByText('Ulcerative Dermatitis').classList).toContainEqual('active');
+  expect(getByText('Features').classList).not.toContainEqual('active');
+  expect(getByText('About').classList).not.toContainEqual('active');
+});
+
+test('Features link is active when current route is /features', () => {
+  const { getByText } = render(HeaderNav, _renderOptions('/features'));
+  expect(getByText('Ulcerative Dermatitis').classList).not.toContainEqual('active');
+  expect(getByText('Features').classList).toContainEqual('active');
+  expect(getByText('About').classList).not.toContainEqual('active');
+});
+
+test('About link is active when current route is /about', () => {
+  const { getByText } = render(HeaderNav, _renderOptions('/about'));
+  expect(getByText('Ulcerative Dermatitis').classList).not.toContainEqual('active');
+  expect(getByText('Features').classList).not.toContainEqual('active');
+  expect(getByText('About').classList).toContainEqual('active');
 });
--- frontend/src/components/header-nav.vue
+++ frontend/src/components/header-nav.vue
@@ -1,40 +1,38 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-
-defineProps({
-  msg: String,
-});
-
-const count = ref(0);
+const props = defineProps<{
+  routePath: string;
+}>();
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code>
-      to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>
-    , the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <nav aria-label="Main menu">
+    <div class="flex h-16 items-center justify-between">
+      <div class="flex-shrink-0">
+        <img class="h-8 w-8 inline-block" src="https://tailwindui.com/img/logos/mark.svg?color=green&shade=500" alt="Vet Lens" />
+        <h1 class="font-bold inline-block pl-2">Vet Lens</h1>
+      </div>
+      <div class="hidden md:block">
+        <div class="ml-10 flex items-baseline space-x-4">
+          <router-link to="/" :class="{ active: routePath === '/' }" class="text-slate-900 hover:bg-green-200 px-3 py-2 rounded-md text-sm font-medium">
+            Ulcerative Dermatitis
+          </router-link>
+          <router-link
+            to="/features"
+            :class="{ active: routePath === '/features' }"
+            class="text-slate-900 hover:bg-green-200 px-3 py-2 rounded-md text-sm font-medium"
+          >
+            Features
+          </router-link>
+          <router-link
+            to="/about"
+            :class="{ active: routePath === '/about' }"
+            class="text-slate-900 hover:bg-green-200 px-3 py-2 rounded-md text-sm font-medium"
+          >
+            About
+          </router-link>
+        </div>
+      </div>
+    </div>
+    <div class="h-px bg-slate-900/5"></div>
+  </nav>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
